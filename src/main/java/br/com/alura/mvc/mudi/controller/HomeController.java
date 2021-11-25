5e3acc269dd3e4b9870dfbbde6d38b5be213c397
Joao-Gontijo <jvgontijo166@gmail.com>
2021-11-25 13:40:06 -0300

    pedido repository

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,31 +1,24 @@
 package br.com.alura.mvc.mudi.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
-import javax.management.Query;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.repositoy.PedidoRepository;
 
 @Controller
 public class HomeController {
 	
-	@PersistenceContext
-	private EntityManager entityManager;
+	@Autowired //Pedir uma instância de pedidoRepository
+	private PedidoRepository repository;
 	
 	@GetMapping("/home")
-	public String home(Model model) {
-		
-		TypedQuery<Pedido> query = entityManager.createQuery("select p from Pedido p", Pedido.class);
-		List<Pedido> pedidos = query.getResultList();
-		
+	public String home(Model model) {	
+		List<Pedido> pedidos = repository.recuperaTodosOsPedidos();
 		model.addAttribute("pedidos", pedidos);
 		return "home";
 	}
