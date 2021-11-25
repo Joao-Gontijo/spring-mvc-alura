93a74ea53425101836a710f444db76dfc7774648
Joao-Gontijo <jvgontijo166@gmail.com>
2021-11-25 15:26:24 -0300

    salvando pedido

--- src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
@@ -1,15 +1,31 @@
 package br.com.alura.mvc.mudi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.alura.mvc.mudi.dto.RequisicaoNovoPedido;
+import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.repositoy.PedidoRepository;
 
 @Controller
 @RequestMapping("/pedido") //mapeia todos os tipos de requisição
 public class PedidoController {
 	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
 	@GetMapping("formulario")
 	public String formulario() {
+		return "pedido/formulario";
+	}
+	
+	@PostMapping("novo")
+	public String novo(RequisicaoNovoPedido requisicao) {
+		Pedido pedido = requisicao.toPedido(); //criar método .toPedido() na dto
+		pedidoRepository.save(pedido);
 		return "pedido/formulario";
 	}
 }
