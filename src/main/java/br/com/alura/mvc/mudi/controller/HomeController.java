08329cab678eadef315907e7d82552f42b6cf158
Joao-Gontijo <jvgontijo166@gmail.com>
2021-11-26 16:40:13 -0300

    paginação com spring data

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -3,6 +3,8 @@ package br.com.alura.mvc.mudi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,7 +23,13 @@ public class HomeController {
 	
 	@GetMapping
 	public String home(Model model) {	
-		List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.ENTREGUE);
+		
+		//resolve a ordem dos itens
+		Sort sort = Sort.by("dataDaEntrega").ascending();
+		
+		PageRequest paginacao = PageRequest.of(0, 10, sort);
+
+		List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.ENTREGUE, paginacao);
 		model.addAttribute("pedidos", pedidos);
 		return "home";
 	}
