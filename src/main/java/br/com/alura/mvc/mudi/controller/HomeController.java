b3e62411fd7db1e8c3338ede47922115ed5b6870
Joao-Gontijo <jvgontijo166@gmail.com>
2021-11-25 21:45:24 -0300

    formatação números e datas

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -32,6 +32,7 @@ public class HomeController {
 	public String aguardando(@PathVariable("status") String status, Model model) {	
 		List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.valueOf(status.toUpperCase()));
 		model.addAttribute("pedidos", pedidos);
+		model.addAttribute("status", status);
 		return "home";
 	}
 	
